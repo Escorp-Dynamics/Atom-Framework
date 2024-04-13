8d946c673c52ab47ab9003121f5cad5279880e02
EXOMODE <exomode@outlook.com>
2024-04-13 19:32:02 +0300

    Добавил механизм отправки сигнала в хост

--- Atom.Web.Browsers/Firefox/Extension/assets/scripts/background/core.js
+++ Atom.Web.Browsers/Firefox/Extension/assets/scripts/background/core.js
@@ -1,1 +1,29 @@
 'use strict';
+
+class Server {
+    constructor(name) {
+        this.port = browser.runtime.connectNative(name);
+    }
+
+    async sendAsync(data) {
+        const json = JSON.stringify(data);
+        await this.port.postMessage(json);
+    }
+
+    async sendSignalAsync(signal, data) {
+        await this.sendAsync({
+            signal: signal,
+            data: data
+        });
+    }
+
+    async sendInstalledSignalAsync() {
+        await this.sendSignalAsync("installed");
+    }
+}
+
+let server = new Server("Atom");
+
+browser.runtime.onInstalled.addListener(async () => {
+  await server.sendInstalledSignalAsync();
+});
